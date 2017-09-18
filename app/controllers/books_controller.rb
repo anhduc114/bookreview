0318a49e5e40e59838c136a34a7e833d6a2667b4
Duc Do <jimraynor1104@gmail.com>
2017-09-18 08:49:09 +0000

    add show books by category

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,15 @@ class BooksController < ApplicationController
     before_action :find_book, only: [:show, :edit, :update, :destroy]
     
     def index
+        if params[:category].blank?
         @books = Book.all.order("created_at DESC")
+    else
+        # sort and show books with perspective id
+        @category_id = Category.find_by(name: params[:category]).id
+        @books = Book.where(:category_id => @category_id).order("created_at DESC")
+        end
     end
-    
+ 
     def show
 
     end
